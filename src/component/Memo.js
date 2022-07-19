1b6a1a864e3d7b224200803ad8a5c469e5ef8afc
sohee kim <sohee063@nate.com>
2022-07-19 17:03:18 +0900

    날씨기능구현

---
+++ src/component/Memo.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+
+const Memo = () => {
+  const [memo, setMemo] = useState("");
+  const [memoList, setMemoList] = useState([]);
+  const onChange = (e) => {
+    setMemo(e.target.value);
+    console.log("memo", memo);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setMemoList(memo);
+  };
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} type="text" />
+        <button type="submit">Add</button>
+      </form>
+      {memoList?.map((el) => (
+        <>
+          <div>{el}</div>
+          <button>dd</button>
+        </>
+      ))}
+    </div>
+  );
+};
+
+export default Memo;
---
+++ src/component/Todo.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Memo from "./Memo";
+
+const Todo = () => {
+  return (
+    <div>
+      <h5>MEMO</h5>
+      <Memo />
+    </div>
+  );
+};
+
+export default Todo;
